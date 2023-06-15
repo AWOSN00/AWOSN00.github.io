2b320620e5615521e1c5e12e14fbc7f238e1a35f
AWOSN00 <136672142+AWOSN00@users.noreply.github.com>
2023-06-14 23:48:47 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhbXFnJu4c3GhL8dOyyJkpllFl_gmEu9c",
+  authDomain: "awosn00.firebaseapp.com",
+  projectId: "awosn00",
+  storageBucket: "awosn00.appspot.com",
+  messagingSenderId: "986545092955",
+  appId: "1:986545092955:web:1c713b7973b2cf0c17827e",
+  measurementId: "G-CXL8P8HTQH"
+};s
